5e337752c3d8ecf2e093e209b028e6c9ebdd65aa
wmf <w15736504930@163.com>
2024-11-29 22:56:32 +0800

    第一次

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)  # 新增字段
+
+    def __str__(self):
+        return self.title
+
+
+from django.db import models
+
 # Create your models here.
